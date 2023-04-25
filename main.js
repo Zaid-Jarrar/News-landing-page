c1ca9d6aae7022ac88eeeb27fdc17632edbdd94a
Zaid-Jarrar <zaid_jarrar@outlook.com>
2023-04-26 01:07:41 +0300

    feature: added swiping to open and close mobile nav

--- main.js
+++ main.js
@@ -10,9 +10,35 @@ toggle?.addEventListener("click", () => {
 });
 
 document.addEventListener("click", (event) => {
-  console.log(event);
   if (!mobNav.contains(event.target) && !toggle.contains(event.target)) {
     mobNav.classList.remove("active");
     backdrop.classList.remove("active");
+    toggle.classList.remove("active");
   }
 });
+
+// when swiping right mobNav will open
+// whe  swiping left mobNav will close
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
+
+function handleTouchStart(event) {
+  // Record the starting X position
+  startX = event.touches[0].clientX;
+}
+
+function handleTouchMove(event) {
+  // Calculate the distance swiped
+  const distance = event.touches[0].clientX - startX;
+
+  if (distance < 0) {
+    // Show the mobile navigation menu if the user swipes to the right by at least 50px
+    mobNav.classList.add("active");
+    backdrop.classList.add("active");
+    toggle.classList.add("active");
+  } else {
+    mobNav.classList.remove("active");
+    backdrop.classList.remove("active");
+    toggle.classList.remove("active");
+  }
+}
